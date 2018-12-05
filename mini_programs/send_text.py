6d3ef9e5ad6fab60bf2948ad011d0eed083acfc2
Pinaki Nath Chowdhury <pinakinathc@users.noreply.github.com>
2018-12-05 14:21:20 +0530

    Update send_text.py

--- mini_programs/send_text.py
+++ mini_programs/send_text.py
@@ -1,12 +1,12 @@
 from twilio import rest
 
-account_sid = "ACa2423bafb67e732cd5d518d045daade2"
-auth_token="0bab2b36624c493efbfdc7db69996884"
+account_sid = "enter your sid"
+auth_token="enter your token"
 client=rest.TwilioRestClient(account_sid,auth_token)
 
 message=client.sms.messages.create(
 	body="Hey this is Pinaki",
-	to="+917432814640",
-	from_="+12016307370")
+	to="twilio account number",
+	from_="number")
 
 print message.sid
